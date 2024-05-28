b7148d2bc16ad6e4c9b58ae7107ec4597e58e28e
zhdanove59 <133035847+zhdanove59@users.noreply.github.com>
2024-05-28 09:52:56 +0200

    wild series done

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// Load environment variables from .env file
+require("dotenv").config();
+
+// Check database connection
+// Note: This is optional and can be removed if the database connection
+// is not required when starting the application
+require("./database/client").checkConnection();
+
+// Import the Express application from app/config.js
+const app = require("./app/config");
+// mon code 
+app.get('/', (req, res) => {
+  res.send('Welcome to Wild Series !');
+});
+// mon code
+// Get the port from the environment variables
+const port = process.env.APP_PORT;
+
+// Start the server and listen on the specified port
+app
+  .listen(port, () => {
+    console.info(`Server is listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error:", err.message);
+  });
